ea67595ff3ec047a8417b99b2916e79150a0b292
subham <majumdersubham14082003@gmail.com>
2025-03-14 19:35:41 +0530

    winner decided

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let buttons = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset-btn");
-
+// console.log(buttons);
+let newgamebtn = document.querySelector("#new-btn");
+let msg = document.querySelector("#msg");
+let msgcontainer = document.querySelector(".msg-container");
 let turn0 = true; // true = o, false = x
 const winpattern = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -10,7 +13,7 @@ const winpattern = [
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        console.log("clicked");
+        // console.log("clicked");
         if (turn0) {
             button.innerHTML = 'O';
             turn0 = false;
@@ -20,6 +23,19 @@ buttons.forEach((button) => {
             turn0 = true;
         }
         button.disabled = true;
+        for(let ind of winpattern){
+            let x = buttons[ind[0]].innerText;
+            let y = buttons[ind[1]].innerText;
+            let z = buttons[ind[2]].innerText;
+            if(x === y && y === z && x !== ""){
+                // console.log("win");
+                msg.innerHTML = `${x} is the winner`;
+                msgcontainer.classList.remove("hide");
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                })
+            }
+        }
     })
 
 })
